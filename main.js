54d7c328ad6b07e8c4e466cce85ca8687b3fc27b
Adem-Maafi <ademmaafi3@gmail.com>
2024-11-14 01:54:38 +0100

    motoshop

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-    var totalAmount = 0; // Variable to hold the total amount
+    var totalAmount = 0; 
 
     function updateWallet() {
-        $('#wallet').text('Wallet: $' + totalAmount.toFixed(2)); // Update wallet with the total amount
+        $('#wallet').text('Wallet: $' + totalAmount.toFixed(2));
     }
 
     $('#btn-honda').click(function () {
         var buttonText = $(this).text();
-        var price = 10000; // Price for Honda CBR1000RR 2019
+        var price = 10000; 
 
         if (buttonText === 'buy') {
             $(this).text('added');
-            $(this).css('background-color', 'green'); // Change button color to green
-            totalAmount += price; // Add price to total
-            updateWallet(); // Update the wallet display
+            $(this).css('background-color', 'green'); 
+            totalAmount += price; 
+            updateWallet(); 
         } else {
             $(this).text('buy');
-            $(this).css('background-color', '#020202'); // Revert back to original color
-            totalAmount -= price; // Subtract price from total
-            updateWallet(); // Update the wallet display
+            $(this).css('background-color', '#020202'); 
+            totalAmount -= price; 
+            updateWallet(); 
         }
     });
 
     $('#btn-kawasaki').click(function () {
         var buttonText = $(this).text();
-        var price = 12000; // Price for Kawasaki Ninja ZX-10R
+        var price = 12000; 
 
         if (buttonText === 'buy') {
             $(this).text('added');
@@ -41,7 +41,7 @@ $(document).ready(function () {
 
     $('#btn-suzuki').click(function () {
         var buttonText = $(this).text();
-        var price = 11000; // Price for Suzuki GSX-S1000S
+        var price = 11000; 
 
         if (buttonText === 'buy') {
             $(this).text('added');
@@ -56,3 +56,5 @@ $(document).ready(function () {
         }
     });
 });
+
+
